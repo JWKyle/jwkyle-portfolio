82567281b77884796c5c87d95175c47271eec21c
Jim Kyle <jim@jwkyle.com>
2018-02-20 19:09:45 -0600

    add basis for page

---
+++ src/assets/main.js
@@ -0,0 +1,68 @@
+$(function() {
+
+  $.ajax({
+  url: 'https://www.codeschool.com/users/jwkyle.json',
+  dataType: 'jsonp',
+  success: function(response) {
+    // console.log('Success!', response.courses.in_progress);
+    completedCourses(response.courses.completed);
+    progressCourses(response.courses.in_progress);
+  }
+});
+
+function completedCourses(courses) {
+
+  var $badges = $('#badges');
+
+  courses.forEach(function(course) {
+    // console.log(course);
+
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />', {
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See Course'
+    }).appendTo($course);
+  })
+}
+function progressCourses(courses) {
+
+  var $progress = $('#progress');
+
+  courses.forEach(function(course) {
+    // console.log(course);
+
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($progress);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />', {
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See Course'
+    }).appendTo($course);
+  })
+}
+
+});
